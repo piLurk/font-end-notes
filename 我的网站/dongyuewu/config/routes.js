94d352c94d4cf9fd1594b70ba02669e2dd5fbc29
wudongyue <wudongyue@jiangroom.com>
2018-03-27 18:37:25 +0800

    js对象描述符 url逗号解析bug promise手动实现

--- 我的网站/dongyuewu/config/routes.js
+++ 我的网站/dongyuewu/config/routes.js
@@ -4,4 +4,14 @@ var Index=require('../controllers/index');
 
 module.exports = function( app ) {
   app.get('/', Index.index)
+
+
+  // ajax
+  app.get('/getJson/test1',function(req, res) {
+    res.json({
+      code:200,
+      message:'请求成功！',
+      data:'这是数据'
+    })
+  })
 }
