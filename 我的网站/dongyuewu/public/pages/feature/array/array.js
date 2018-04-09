9d7420216664bdf8d7f24c729fa0c16abce2cc93
wudongyue <wudongyue@jiangroom.com>
2018-04-09 18:12:20 +0800

    定时器与线程 数组

--- 我的网站/dongyuewu/public/pages/feature/array/array.js
+++ 我的网站/dongyuewu/public/pages/feature/array/array.js
@@ -16,6 +16,15 @@
 var arr = [1,2,3,4];
 arr.fill('test',2,5)
 
+// reduce
+const numbers = [10,20,30,40]
+const doubleOver50 = numbers.reduce( (finalList, num) => {
+    num *= 2;
+    if(num > 50) {
+        finalList.push(num)
+    }
+    return finalList
+}, [])
 
 // 数组与set集合
     // 去重的一种实现
