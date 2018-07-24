8c521c5190a8b83399fcac02e4ff588aeaef2262
wudongyue <wudongyue@jiangroom.com>
2018-07-24 09:45:21 +0800

    单例模式

---
+++ 我的网站/dongyuewu/public/pages/workout/designMode.js
@@ -0,0 +1,21 @@
+const singleton = function(name) {
+  this.name = name
+  this.instance = null
+}
+
+singleton.prototype.getName = function() {
+  console.log(this.name)
+}
+
+singleton.getInstance = function(name) {
+  let n = name;
+  if (!this.instance) { // 关键语句
+    this.instance = new singleton(n)
+  }
+  return this.instance
+}
+
+// test
+const a = singleton.getInstance('a') // 通过 getInstance 来获取实例
+const b = singleton.getInstance('b')
+console.log(a === b)
